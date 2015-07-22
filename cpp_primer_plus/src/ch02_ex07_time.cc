2ebcf4141ebd2831a8afa136cb5b80f2c9979d8e
xtopsoft <xtopsoft@163.com>
2015-07-22 15:58:32 +0800

    workend

---
+++ cpp_primer_plus/src/ch02_ex07_time.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    
+	int hour, minute;
+
+	cout << "Enter the number of hours: ";
+	cin >> hour;
+	cout << "Enter the number of minutes: ";
+	cin >> minute;
+	cout << "Time: " << hour << ":" << minute << endl;
+    
+    return 0;
+}
